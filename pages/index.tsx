c8b7d19c26eb2b6306459dfb5913e1605b5dce18
Berk Safran <berksafranbolulu@gmail.com>
2021-11-27 21:30:09 +0300

    Initial commit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,11 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h1 className="text-2xl text-red-500
+        ">
+          Tailwind...
+        </h1>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
